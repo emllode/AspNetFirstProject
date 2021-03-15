03716d7c82a7fd09b0f02957a5a19d41c4d81c07
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-03-15 22:46:24 +0100

    Fixed Models for my database for Attendee, event, organizer.

---
+++ razorapp1/Models/Attendee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace razorapp1.Models
+{
+    public class Attendee
+    {
+        public int ID { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
---
+++ razorapp1/Models/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace razorapp1.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public List<Organizer> Organizer { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Address { get; set; }
+        public int SpotsAvailable { get; set; }
+
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        
+    }
+}
