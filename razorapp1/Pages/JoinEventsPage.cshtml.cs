2cad60f9957b13f2b1baa0837a171799ed8f0a21
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-03-15 22:31:54 +0100

    First commit

---
+++ razorapp1/Pages/JoinEventsPage.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razorapp1.Pages
+{
+    public class JoinEventsPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
